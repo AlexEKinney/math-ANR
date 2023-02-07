cf3e84dace18e4e60824d929de5360199e8a1b1d
SightedBike273 <sighted@kincode.net>
2023-02-07 17:46:41 +0000

    homework act

--- app/src/main/java/net/kincode/maths/TaskActivity.kt
+++ app/src/main/java/net/kincode/maths/TaskActivity.kt
@@ -1,4 +1,15 @@
 package net.kincode.maths
 
-class TaskActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class TaskActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        auth = FirebaseAuth.getInstance()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task)
+    }
+
 }
